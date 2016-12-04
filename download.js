825e74d8f41c0c4e1c6849fd4642665ec48338fb
Club <cjjessie793@gmail.com>
2016-12-03 22:55:46 -0500

    Added save function.

--- download.js
+++ download.js
@@ -3,4 +3,25 @@ document.getElementById("download_music").addEventListener("click", getObjects);
 
 function getObjects() {
 	var audioObjects = getAudioPlayer()._currentAudio[2];
+	
+	saveAs(audioObjects);
+}
+
+function saveAs(uri) {
+    var link = document.createElement('a');
+    if (typeof link.download === 'string') {
+        link.href = uri;
+        link.setAttribute('download', true);
+
+        //Firefox requires the link to be in the body
+        document.body.appendChild(link);
+
+        //simulate click
+        link.click();
+
+        //remove the link when done
+        document.body.removeChild(link);
+    } else {
+        window.open(uri);
+    }
 }
